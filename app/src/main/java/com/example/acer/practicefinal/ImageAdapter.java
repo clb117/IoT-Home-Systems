fc056417e42725c744ec51f99541883ee0183252
Chelsey Bryant <clb117@case.edu>
2015-11-21 15:18:18 -0500

    First commit

---
+++ app/src/main/java/com/example/acer/practicefinal/ImageAdapter.java
@@ -0,0 +1,57 @@
+package com.example.acer.practicefinal;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+/**
+ * CB A class to generate the images on the GridView
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public ImageAdapter (Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        // insert on click listener here imageView.setOnClickListener();
+        return imageView;
+    }
+
+
+    // references to our images
+    private Integer[] mThumbIds = {
+           //CB this will contain the images read from the CSV file which is saved in the create form
+    };
+}
