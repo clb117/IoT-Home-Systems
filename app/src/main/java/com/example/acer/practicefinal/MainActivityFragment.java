7a69e7ca8e95b29f1a1d923b59b1f7a9534402d7
Ted <tkk21@case.edu>
2015-11-25 20:40:58 -0500

    use the proper fragment

--- app/src/main/java/com/example/acer/practicefinal/MainActivityFragment.java
+++ app/src/main/java/com/example/acer/practicefinal/MainActivityFragment.java
@@ -1,11 +1,8 @@
 package com.example.acer.practicefinal;
 
-import android.app.Fragment;
-import android.media.Image;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
