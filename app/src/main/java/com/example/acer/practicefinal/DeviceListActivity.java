8402fec353f24a3b91edd8f82a8603e940bd7b1a
Ted <tkk21@case.edu>
2015-11-28 22:21:52 -0500

    added in list activity

---
+++ app/src/main/java/com/example/acer/practicefinal/DeviceListActivity.java
@@ -0,0 +1,14 @@
+package com.example.acer.practicefinal;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class DeviceListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+
+}
